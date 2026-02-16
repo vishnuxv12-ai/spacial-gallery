225b192014cb05de84a3ab522d521c02e95c474a
vishnuxv12-ai <vishnuxv1512@gmail.com>
2026-02-16 15:54:33 +0530

    Delete gallery-data.js

--- gallery-data.js
+++
@@ -1,5 +0,0 @@
-// --- SAVEPOINT 4 ---
-window.galleryData = {
-  // 50 Placeholder Images from Picsum
-  images: Array.from({ length: 50 }, (_, i) => `https://picsum.photos/200/300?random=${i + 1}`)
-};
